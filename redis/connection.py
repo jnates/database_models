ac7e92daaaa67e3d75c3926af12953745443ad7e
Juan Nates <nates1999123@gmail.com>
2021-12-14 19:47:22 -0500

    added-changes-for-new-connection

--- redis/connection.py
+++ redis/connection.py
@@ -1,12 +1,15 @@
 import redis
-
-redis_host = '127.0.0.1'
-redis_port = 6379
-password = '1234'
+import os 
+from dotenv import load_dotenv
+load_dotenv()
 
 def redis_string():
     try:
-        r = redis.Redis(host=redis_host, port=redis_port, db=0, password=password)
+        r = redis.Redis(host = os.getenv("REDIS_HOST"), 
+                        port = os.getenv("REDIS_PORT"), 
+                        db = os.getenv("REDIS_DB"), 
+                        password = os.getenv("REDIS_PASSWORD"),
+                        )
         r.set("mensaje","test")
         msg = r.get("mensaje")
         print(msg)
