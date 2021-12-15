2c9356f2878bd33c74d812dd262f3d0af840c63d
Juan Nates <nates1999123@gmail.com>
2021-12-14 19:13:37 -0500

    added-changes-for-new-connection-in-redis

--- redis/connection.py
+++ redis/connection.py
@@ -1,5 +1,17 @@
 import redis
 
-r = redis.Redis(host='127.0.0.1', port=6379, db=0, password="1234")
-r.set('name', 'test')
-print(r.get('name'))
+redis_host = '127.0.0.1'
+redis_port = 6379
+password = '1234'
+
+def redis_string():
+    try:
+        r = redis.Redis(host=redis_host, port=redis_port, db=0, password=password)
+        r.set("mensaje","test")
+        msg = r.get("mensaje")
+        print(msg)
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    redis_string()
