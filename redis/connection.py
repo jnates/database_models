eb1e31364475711fffc0d7c51fe13f18051e182c
Juan Nates <nates1999123@gmail.com>
2021-12-14 22:14:58 -0500

    added-changes

--- redis/connection.py
+++ redis/connection.py
@@ -1,18 +1,23 @@
 import redis
 import os 
 from dotenv import load_dotenv
+from faker import Faker
 load_dotenv()
 
 def redis_string():
     try:
+        fake = Faker('it_IT')
         r = redis.Redis(host = os.getenv("REDIS_HOST"), 
                         port = os.getenv("REDIS_PORT"), 
                         db = os.getenv("REDIS_DB"), 
                         password = os.getenv("REDIS_PASSWORD"),
                         )
-        r.set("mensaje","test")
-        msg = r.get("mensaje")
-        print(msg)
+        for _ in range(10):
+            r.sadd("message",fake.name())
+            r.sadd("addres",fake.address())
+#            r.flushall()
+            msg = r.smembers("message")
+            print(msg)
     except Exception as e:
         print(e)
 
